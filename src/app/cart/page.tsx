35f288c49196e7a5d7e8e37faa3e71485a6a9420
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-11 00:05:36 +0300

    The user's cart

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPage = () =>
+    {
+        return (
+            <div>This page is to include the items in the user's Cart</div>
+        )
+    }
+
+    export default CartPage;
+    
