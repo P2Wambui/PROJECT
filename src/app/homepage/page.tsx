9125695988a32d8e7b590867bbfb70858dd9b26a
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-11 00:07:18 +0300

    Create page.tsx

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Featured from '@/components/Featured'
+import Offer from '@/components/Offer'
+
+
+const Homepage = () =>
+    {
+        return (
+            <main>
+                <Featured/>
+                <Offer/>
+               
+            </main>
+
+        )
+    }
+
+    export default Homepage;
