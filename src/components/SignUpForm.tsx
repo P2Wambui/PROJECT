0cb983fbd811279aa83b375b89cb3309d3bc8c7b
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-21 11:08:44 +0300

    Create SignUpForm.tsx

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useState } from "react";
+import React from "react";
+import Link from "next/link";
+
+const SignUpForm = () =>
+    {
+        const [email, setEmail]= useState('')
+        const [phonenumber, setPhonenumber]= useState('')
+        const [password, setPassword]= useState('')
+
+        return (
+        
+            <form className="">
+                <label>
+                    <span>Email:</span>
+                    <input required type="text" onChange={(e) => setEmail(e.target.value)} value={email} 
+                    className="bg-gray-200 placeholder:text-gray-400 p-3 rounded" placeholder="joe@gmail.com"/>
+                 
+                </label>
+                  <br/>
+                  <br/>
+                <label>
+                    <span>Phone Number:</span>
+                    <input required type="text" onChange={(e) => setPhonenumber(e.target.value)} value={phonenumber}
+                     className="bg-gray-200 placeholder:text-gray-400 p-3 rounded"placeholder="01234567"/>
+                </label>
+                <br/>
+                <br/>
+                <label>
+                    <span>Password:</span>
+                    <input required type="password" onChange={(e) => setPassword(e.target.value)} value={password}
+                     className="bg-gray-200 placeholder:text-gray-400 p-3 rounded" placeholder="password"/>
+                </label>
+                <br/>
+                <br/>
+
+        {/*USE A BUTTON INSTEAD OF LINK*/}    
+                <Link className=" text-white flex justify-center py-4 px-8 rounded-md ring-black bg-rose-700" href="/residents">Submit</Link>
+            </form>
+            
+        )
+    }
+
+    export default SignUpForm;
