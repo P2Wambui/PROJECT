75574add47decf6237d559d416c7323c6e5b3ec1
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-21 10:29:43 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,33 +8,40 @@ const LoginPage = () =>
     {
         return (
 
-            <div className=" text-black font-bold p-4  h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+            <div className=" text-black bg-fuchsia-50 font-bold p-4  h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
            
             {/* BOX */}
-            <div className=" h-full shadow-2xl rounded-md md:flex-row md:h-[100%] md:w-full lg:w-[90%] 2xl:w-1/3 bg-blue-100">
+            <div className=" h-full shadow-2xl rounded-md md:flex-row md:h-[100%] md:w-full lg:w-[90%] 2xl:w-1/3">
            
-              {/* IMAGE CONTAINER */}
+             
            
               {/* FORM CONTAINER */}
               <div className="p-10 md:w-full gap-8">
-
-                <h1 className="font-bold text-xl xl:text-3xl">Welcome Back!</h1>
-                <br/>
-                <h2 className="font-bold">Log In to continue</h2>
+                <h1 className="font-bold text-xl xl:text-3xl text-center">Welcome Back!</h1>
+                
+                <h2 className="font-bold underline text-center">Log In to continue</h2>
                 <br/>
                 <LogInForm/>
-                <br/>
-                
-                <p>------------OR-------------</p>
-                <br/>
+                 <br/>
+                <div className="flex justify-center">
+                   <span>OR</span>
+                 </div>
+                <br/> 
+               
+                <div className="flex justify-center">
                 <button className="flex gap-4 p-4 ring-1 ring-black rounded-md">
                   <Image src="/google.png" alt="" width={30} height={30} className="object-contain"/>
-                  <span className="font-bold">Sign in with Google</span>
+                  <span className="font-bold">Sign Up with Google</span>
                 </button>  
-                <br/>
+                </div>
+                 <br/>
+
+                 <div  className="flex justify-center">
                 <p className="text-sm">
-                  Have a problem?<Link className="underline" href="/"> Contact us</Link>
+                  Have a problem?<Link className="underline" href="/">Contact us</Link>
                 </p>
+                 </div> 
+              
               
               </div>
             </div>
