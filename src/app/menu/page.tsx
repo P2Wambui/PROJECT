521494da969565d9b6fcbd4c00d769e5ab45181c
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-21 10:31:00 +0300

    Create page.tsx

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+
+const Menupage = () =>
+    {
+        return(
+           <main>
+             <Featured/>
+             <Offer/>
+           </main> 
+        )
+    }
+    export default Menupage;
