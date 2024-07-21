d3074e01ad272b86cf67f7ca03a9e6f24aabfbf5
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-21 10:34:14 +0300

    Create page.tsx

---
+++ src/app/residents/page.tsx
@@ -0,0 +1,15 @@
+import Featured from '@/components/Featured';
+import Offer from '@/components/Offer';
+import React from 'react';
+
+const Residentpage = () => {
+  return (
+    <main>
+        <Featured/>
+        <Offer/>
+
+    </main>
+  )
+}
+
+export default Residentpage;
