a44654474a6d2af72ccc2af78e19d99d042e77c2
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-21 11:02:16 +0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer = () =>
+    {
+        return (
+            <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-rose-700 flex items-center justify-between">
+            <Link href="/" className="font-bold text-xl">The Food-Ferry</Link>
+            <p>© ALL RIGHTS RESERVED.</p>
+          </div>
+        )
+    }
+
+    export default Footer;
