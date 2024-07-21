783daa84d9991091d4364d4daf035c239f8bbbdf
Wambui <167061736+P2Wambui@users.noreply.github.com>
2024-07-21 11:04:59 +0300

    Create Notification.tsx

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notification = () =>
+    {
+        return (
+            <div className="h-12 bg-rose-800 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+                Free delivery for orders above Ksh. 2,000!
+                </div>
+        )
+    }
+
+    export default Notification;
